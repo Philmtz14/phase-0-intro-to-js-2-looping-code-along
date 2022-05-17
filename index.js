593288b7ad52b3c04882a2bbdc2101fa2b7f4540
philip martinez <martinez.philip00@gmail.com>
2022-05-17 14:17:54 -0500

    commit tree b8d8aee4dd47938ab596d89a6c849080263c0feb

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards (cards, surprise) {
+    let firstArr = [];
+    for (let i = 0; i < cards.length; i++){
+        firstArr.push(`Thank you, ${cards[i]}, for the wonderful ${surprise} gift!`);
+    }
+    return firstArr;
+}
+
+function countDown () {
+    let number = 11;
+    while (number > 0) {
+        number--;
+        console.log(number);
+    }
+}
